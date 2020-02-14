725f70282933c7a9bf090744deed5fafdd426e9c
yqiu23 <60491664+yqiu23@users.noreply.github.com>
2020-02-13 20:31:43 -0600

    Add files via upload

---
+++ hw2-yqiu23/lib361.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "lib361.h"
+
+int secretoperation(int a, int b){
+   int c = a + b + 1;
+   return c;
+}
+
---
+++ hw2-yqiu23/lib361.h
@@ -0,0 +1,8 @@
+#ifndef lib361__h__
+#define lib361__h__
+#include <stdio.h>
+
+
+int secretoperation(int a, int b);
+
+#endif 
